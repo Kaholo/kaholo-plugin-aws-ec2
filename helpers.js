04128803ac88725e962bd02e0fdac56082cf253e
arispen <arispen@gmail.com>
2022-02-15 11:50:10 +0100

    KP-559 [AWS EC2] [Modify Instance Attribute] (new method) (#20)
    * modifyInstanceAttribute WIP
    
    * add eslint and jest, fix autofixable problems
    
    * fixed all issues in app.js
    
    * fixed all eslint issues
    
    * clean up modifyInstanceAttribute
    
    * review fixes
    
    * append review fix

--- helpers.js
+++ helpers.js
@@ -1,90 +1,96 @@
 const aws = require("aws-sdk");
-const parsers = require("./parsers")
+const parsers = require("./parsers");
 
 function getEc2(params, settings) {
-    return new aws.EC2({
-        region: parsers.autocomplete(params.REGION),
-        accessKeyId: params.AWS_ACCESS_KEY_ID || settings.AWS_ACCESS_KEY_ID,
-        secretAccessKey: params.AWS_SECRET_ACCESS_KEY || settings.AWS_SECRET_ACCESS_KEY
-    });
+  return new aws.EC2({
+    region: parsers.autocomplete(params.REGION),
+    accessKeyId: params.AWS_ACCESS_KEY_ID || settings.AWS_ACCESS_KEY_ID,
+    secretAccessKey: params.AWS_SECRET_ACCESS_KEY || settings.AWS_SECRET_ACCESS_KEY,
+  });
 }
 
 function getLightsail(params, settings) {
-    return new aws.Lightsail({
-        region: parsers.autocomplete(params.REGION),
-        accessKeyId: params.AWS_ACCESS_KEY_ID || settings.AWS_ACCESS_KEY_ID,
-        secretAccessKey: params.AWS_SECRET_ACCESS_KEY || settings.AWS_SECRET_ACCESS_KEY
-    });
+  return new aws.Lightsail({
+    region: parsers.autocomplete(params.REGION),
+    accessKeyId: params.AWS_ACCESS_KEY_ID || settings.AWS_ACCESS_KEY_ID,
+    secretAccessKey: params.AWS_SECRET_ACCESS_KEY || settings.AWS_SECRET_ACCESS_KEY,
+  });
 }
 
-async function runEc2Func(action, settings, params, funcName){
-    action.params.ec2 = action.params.ec2 ? action.params.ec2 : getEc2(action.params, settings);
-    const resultPromise = new Promise((resolve, reject) => {
-        action.params.ec2[funcName](params, (err, result) => {
-            if (err) reject(err);
-            else resolve(result);
-        });
+async function runEc2Func(action, settings, params, funcName) {
+  const actionCopy = { ...action };
+  actionCopy.params.ec2 = actionCopy.params.ec2 || getEc2(actionCopy.params, settings);
+  const resultPromise = new Promise((resolve, reject) => {
+    actionCopy.params.ec2[funcName](params, (err, result) => {
+      if (err) { reject(err); } else { resolve(result); }
     });
-    const result = {};
-    result[funcName] = await resultPromise;
-    return result;
+  });
+  const result = {};
+  result[funcName] = await resultPromise;
+  return result;
 }
 
-async function wairForEc2Resource(action, state, params){
-    const ec2 = action.params.ec2 ? action.params.ec2 : getEc2(action.params, settings);
-    return new Promise((resolve, reject) => {
-        ec2.waitFor(state, params, (err, result) => {
-            if (err) return reject(err);
-            resolve(result);
-        });
+async function waitForEc2Resource(action, state, params) {
+  const ec2 = action.params.ec2 || getEc2(action.params);
+  return new Promise((resolve, reject) => {
+    ec2.waitFor(state, params, (err, result) => {
+      if (err) { return reject(err); }
+      return resolve(result);
     });
+  });
 }
 
 function parseLegacyParam(param, parseFunc) {
-    try {
-        if (typeof param == 'string') return JSON.parse(param);
+  let parsedParam;
+  try {
+    if (typeof param === "string") {
+      parsedParam = JSON.parse(param);
+    } else {
+      parsedParam = parseFunc(param);
     }
-    catch (err) {}
-    finally {
-        return parseFunc(param);
-    }
+  } catch (err) {
+    throw new Error("Error parsing legacy param");
+  }
+  return parsedParam;
 }
 
-function getPortObj(fromPort, toPort, ipProtocol, cidrIps, cidrIps6, description){
-    const obj = {
-        FromPort: fromPort,
-        ToPort: toPort,
-        IpProtocol: ipProtocol
-    };
-    if (cidrIps6.length > 0){
-        obj.Ipv6Ranges = cidrIps6.map((CidrIpv6) => ({
-            CidrIpv6,
-            Description: description
-        }));
-    }
-    if (cidrIps.length > 0){
-        obj.IpRanges = cidrIps.map((CidrIp) => ({
-            CidrIp,
-            Description: description
-        }));
-    }
-    return obj;
+function getPortObj(fromPort, toPort, ipProtocol, cidrIps, cidrIps6, description) {
+  const obj = {
+    FromPort: fromPort,
+    ToPort: toPort,
+    IpProtocol: ipProtocol,
+  };
+  if (cidrIps6.length > 0) {
+    obj.Ipv6Ranges = cidrIps6.map((CidrIpv6) => ({
+      CidrIpv6,
+      Description: description,
+    }));
+  }
+  if (cidrIps.length > 0) {
+    obj.IpRanges = cidrIps.map((CidrIp) => ({
+      CidrIp,
+      Description: description,
+    }));
+  }
+  return obj;
 }
 
-async function waitForNatGateway(action, settings){
-    const params = {NatGatewayIds: [action.params.natGatewayId]};
-    let result;
-    while (!result || result.describeNatGateways.NatGateways[0].State !== "available"){
-        result = await runEc2Func(action, settings, params, "describeNatGateways");
-    }
+async function waitForNatGateway(action, settings) {
+  const params = { NatGatewayIds: [action.params.natGatewayId] };
+  let result;
+  while (!result || result.describeNatGateways.NatGateways[0].State !== "available") {
+    // TODO: take a look if this loop is really needed
+    // eslint-disable-next-line no-await-in-loop
+    result = await runEc2Func(action, settings, params, "describeNatGateways");
+  }
 }
 
 module.exports = {
-    getEc2,
-    getLightsail,
-    runEc2Func,
-    parseLegacyParam,
-    getPortObj,
-    waitForNatGateway,
-    wairForEc2Resource
-}
+  getEc2,
+  getLightsail,
+  runEc2Func,
+  parseLegacyParam,
+  getPortObj,
+  waitForNatGateway,
+  waitForEc2Resource,
+};
--- parsers.js
+++ parsers.js
@@ -1,72 +1,104 @@
-function parseArray(value){
-    if (!value) return [];
-    if (Array.isArray(value)) return value;
-    if (typeof(value) === "string") return value.split("\n").map(line=>line.trim()).filter(line=>line);
-    throw "Unsupprted array format";
+const _ = require("lodash");
+
+function parseArray(value) {
+  if (!value) {
+    return [];
+  }
+  if (Array.isArray(value)) {
+    return value;
+  }
+  if (typeof (value) === "string") {
+    return value.split("\n").map((line) => line.trim()).filter((line) => line);
+  }
+  throw new Error("Unsupported array format");
 }
 
-function parseTags(value){
-    if (!value) return undefined;
-    if (Array.isArray(value)){
-        if (!value.every(tag => tag.Key)){
-            throw "Bad AWS Tags Format";
-        }
-        return value;
+function parseTags(value) {
+  let valueCopy = _.clone(value);
+  if (!valueCopy) {
+    return undefined;
+  }
+  if (Array.isArray(valueCopy)) {
+    if (!valueCopy.every((tag) => tag.Key)) {
+      throw new Error("Bad AWS Tags Format");
     }
-    if (typeof(value) === "string"){
-        value = value.split("\n").map(line=>line.trim()).filter(line=>line);
-        return value.map((line) => {
-            let [key, ...val] = line.split("=");
-            if (Array.isArray(val)){
-                val = val.join("=");
-            }
-            return { Key: key, Value: val };
-        });
-    }
-    if (typeof(value) === "object"){
-        return Object.entries(value).map(([key, val])=>{
-            return { Key: key, Value: val };
-        });
-    }
-    throw "Unsupported tags format!";
+    return valueCopy;
+  }
+  if (typeof (valueCopy) === "string") {
+    valueCopy = valueCopy.split("\n").map((line) => line.trim()).filter((line) => line);
+    return valueCopy.map((line) => {
+      const [key, ...val] = line.split("=");
+      let stringValue;
+      if (Array.isArray(val)) {
+        stringValue = val.join("=");
+      } else {
+        stringValue = val;
+      }
+      return { Key: key, Value: stringValue };
+    });
+  }
+  if (typeof (valueCopy) === "object") {
+    return Object.entries(valueCopy).map(([key, val]) => ({ Key: key, Value: val }));
+  }
+  throw new Error("Unsupported tags format!");
 }
 
 module.exports = {
-    boolean : (value) =>{
-        if (value === undefined || value === null || value === '') return undefined;
-        return !!(value && value !=="false")
-    },
-    text : (value) =>{
-        if (value)
-            return value.split('\n');
-        return undefined;
-    },
-    environmentVariables : (value)=>{
-        const parsedEnvironmentVariables = module.exports.text(value);
-        return !parsedEnvironmentVariables ? undefined : parsedEnvironmentVariables.reduce((obj, rawVariable)=>{
-            const [key, value] = rawVariable.split(':');
-            obj[key.trim()] = value.trim();
-            return obj;
-        },{})
-    },
-    number: (value)=>{
-        if (!value) return undefined;
-        const parsed = parseInt(value);
-        if (parsed === NaN) {
-            throw `Value ${value} is not a valid number`
-        }
-        return parsed;
-    },
-    autocomplete: (value)=>{
-        if (!value) return undefined;
-        if (value.id) return value.id;
-        return value;
-    },
-    string: (value)=>{
-        if (!value) return undefined;
-        if (typeof(value) === "string") return value.trim();
-        throw `Value ${value} is not a valid string`;
-    },
-    array: parseArray,
-    tags: parseTags
-}
+  boolean: (value) => {
+    if (value === undefined || value === null || value === "") {
+      return undefined;
+    }
+    return !!(value && value !== "false");
+  },
+  text: (value) => {
+    if (value) {
+      return value.split("\n");
+    }
+    return undefined;
+  },
+  environmentVariables: (value) => {
+    const valueCopy = _.clone(value);
+    const parsedEnvironmentVariables = module.exports.text(valueCopy);
+    if (!parsedEnvironmentVariables) {
+      return undefined;
+    }
+
+    return parsedEnvironmentVariables.reduce((obj, rawVariable) => {
+      const [k, v] = rawVariable.split(":");
+      return {
+        ...obj,
+        [k.trim()]: v.trim(),
+      };
+    }, {});
+  },
+  number: (value) => {
+    if (!value) {
+      return undefined;
+    }
+    const parsed = parseInt(value, 10);
+    if (Number.isNaN(parsed)) {
+      throw new Error(`Value ${value} is not a valid number`);
+    }
+    return parsed;
+  },
+  autocomplete: (value) => {
+    if (!value) {
+      return undefined;
+    }
+    if (value.id) {
+      return value.id;
+    }
+    return value;
+  },
+  string: (value) => {
+    if (!value) {
+      return undefined;
+    }
+    if (typeof (value) === "string") {
+      return value.trim();
+    }
+    throw new Error(`Value ${value} is not a valid string`);
+  },
+  array: parseArray,
+  tags: parseTags,
+};
