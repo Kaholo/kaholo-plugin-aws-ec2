f8d5820e9ddfc832982352948d26a51094994477
xkcm <xkcm16@gmail.com>
2022-04-06 11:05:43 +0200

    Display SubnetName and AvailabilityZone in listSubnets autocomplete

--- autocomplete.js
+++ autocomplete.js
@@ -22,12 +22,21 @@ async function getInstanceTypes(query, params, client, region) {
 
 async function listSubnets(query, params, client) {
   const subnets = await client.describeSubnets().promise();
+  const createSubnetText = (subnet) => {
+    const textSegments = [subnet.SubnetId];
+    const subnetName = subnet.Tags.find(({ Key }) => Key === "Name");
+    if (subnetName) {
+      textSegments.push(subnetName.Value);
+    }
+    textSegments.push(subnet.AvailabilityZone);
+    return textSegments.join(" | ");
+  };
 
   return subnets.Subnets.filter(
     (subnet) => subnet.VpcId.includes(query)
       || subnet.AvailabilityZone.includes(query)
       || subnet.SubnetId.includes(query),
-  ).map((subnet) => autocomplete.toAutocompleteItemFromPrimitive(subnet.SubnetId));
+  ).map((subnet) => autocomplete.toAutocompleteItemFromPrimitive(createSubnetText(subnet)));
 }
 
 module.exports = {
